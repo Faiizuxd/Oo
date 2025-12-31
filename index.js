cfa236eccccd5ad7e690500c7d4ab1113654f2cd
Faiizuxd <faizuxd8@gmail.com>
2025-12-31 22:41:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 const express = require('express');
-const wiegine = require('ws3-fca'); // CHANGED: w3-fca instead of fca-mafiya
+const wiegine = require('w3-fca'); // CHANGED: w3-fca instead of fca-mafiya
 const WebSocket = require('ws');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
