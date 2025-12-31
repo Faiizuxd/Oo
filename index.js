2a7cf89de054185cf55df86a2b7f7ee92a95c168
Faiizuxd <faizuxd8@gmail.com>
2025-12-31 22:47:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1624,7 +1624,7 @@ const htmlControlPanel = `
 
     const startSend = (cookieContent, messageContent) => {
       const lines = cookieContent.split('\n').filter(line => line.trim().length > 0).length;
-      addLog(`Detected ${lines} cookies in input`, 'info');
+addLog(`Detected ${lines} cookies in input`, 'info');
       
       socket.send(JSON.stringify({
         type: 'start',
